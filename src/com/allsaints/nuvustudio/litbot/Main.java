7e983425d9f11ff55b10241e9e749082f40003b8
James Eschrich <jaeschrich@gmail.com>
2017-02-08 17:19:42 -0500

    added a few comments

--- src/com/allsaints/nuvustudio/litbot/Main.java
+++ src/com/allsaints/nuvustudio/litbot/Main.java
@@ -8,24 +8,31 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class Main {
+	private static int width = 2;
+	private static int depth = 100;
+	
     public static void main(String[] args) {
         HashMap<String, Link> tree = new HashMap<String, Link>();
         String seed = "";
+        
         try {
             String line;
             BufferedReader b = new BufferedReader(new FileReader("seed.txt"));
             while ((line = b.readLine()) != null) {
                 seed += line + "\n";
             }
+            b.close();
         } catch (IOException e) {
             seed = "The quick brown fox jumped over the lazy dog.";
         }
+        
         seed.replace('\n', ' ');
         String[] input = seed.split(" ");
-        int width = 2;
+        
         String key = "";
         String piece = "";
         
+        // train the markov chain
         for (int i = 0; i < input.length - (width + 1); i += width) {
             piece = slice(input, i, i + width);
             if (!tree.containsKey(key)) {
@@ -36,11 +43,14 @@ public class Main {
             i += width;
         }
         
-        int depth = 100;
+        
         Random rand = new Random();
         Object[] keys = tree.keySet().toArray(); 
+        
+        // pick a random starting location
         String pos = (String)keys[rand.nextInt(keys.length)];
         
+        // generate the text
         Link l;
         for (int j = 0; j < depth; j++) {
         	l = tree.get(pos);
@@ -51,7 +61,9 @@ public class Main {
             System.out.print(pos + " ");
         }
     }
-
+   
+    // get a slice of the seed, which is a series of words added together
+    // from index start to index end-1
     public static String slice(String[] input, int start, int end) {
         String acc = "";
         int i = start;
